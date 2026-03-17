bc7db3123d92186daa3cadc6db16a771ac8ab831
Muhammad Hassan <immaharhasaan@gmail.com>
2026-03-17 11:18:43 +0500

    Update code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:mind_arena/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MindArenaApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
