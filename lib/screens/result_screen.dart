9042f278890ff203d67809b998295db676ac02bb
Muhammad Hassan <immaharhasaan@gmail.com>
2026-03-18 11:58:37 +0500

    Update code

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
+  // Added const constructor with key
+  const ResultScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Result")),
+      appBar: AppBar(title: const Text("Result")),
 
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
 
-            Text("🎉 Result", style: TextStyle(fontSize: 24)),
-            Text("Rank #3"),
-            Text("You won 200 PKR"),
+            const Text(
+              "🎉 Result",
+              style: TextStyle(fontSize: 24),
+            ),
+
+            const SizedBox(height: 10),
+
+            const Text("Rank #3"),
+
+            const SizedBox(height: 5),
+
+            const Text("You won 200 PKR"),
 
           ],
         ),
