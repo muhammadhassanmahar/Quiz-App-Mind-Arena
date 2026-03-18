79e38ba56210bf212f1b34ed4a2f26c44e6a7754
Muhammad Hassan <immaharhasaan@gmail.com>
2026-03-18 11:47:26 +0500

    Update code

--- lib/screens/contest_screen.dart
+++ lib/screens/contest_screen.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'quiz_screen.dart';
 
 class ContestScreen extends StatelessWidget {
-
   final int fee;
 
-  ContestScreen({required this.fee});
+  // Added const constructor + key
+  const ContestScreen({super.key, required this.fee});
 
   @override
   Widget build(BuildContext context) {
@@ -17,15 +17,21 @@ class ContestScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
 
-            Text("Players: 10/20"),
+            const Text("Players: 10/20"),
+
+            const SizedBox(height: 20),
 
             ElevatedButton(
-              child: Text("Join Contest"),
-              onPressed: (){
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => QuizScreen()));
+              child: const Text("Join Contest"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const QuizScreen(),
+                  ),
+                );
               },
-            )
+            ),
 
           ],
         ),
