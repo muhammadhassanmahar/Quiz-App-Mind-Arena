fb6d70b1feaa03be93418cf33656187c0d47d593
Muhammad Hassan <immaharhasaan@gmail.com>
2026-03-18 13:34:02 +0500

    Update code

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,25 @@ import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 
 void main() {
-  runApp(MindArenaApp());
+  runApp(const MindArenaApp());
 }
 
 class MindArenaApp extends StatelessWidget {
+  // Added const constructor with key
+  const MindArenaApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Mind Arena",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginScreen(),
+
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+
+      home: const LoginScreen(),
     );
   }
 }
